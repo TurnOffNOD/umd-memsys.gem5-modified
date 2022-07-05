9f1143e5f8f689b228336b19fc78b3ab3e86dc8e
Thomas Copper <universeCopper@gmail.com>
2022-07-05 18:56:26 +0800

    tutorial debugging. This commit always can't pass style checker? Have to commit with --no-verify

--- src/tutorial/hello_object.cc
+++ src/tutorial/hello_object.cc
@@ -1,11 +1,13 @@
-#include "tutorial/hello_object.hh"
-
 #include <iostream>
+
+#include "debug/Hello.hh"
+#include "tutorial/hello_object.hh"
 
 HelloObject::HelloObject(HelloObjectParams *params) :
         SimObject(params)
 {
-        std::cout << "Hello World from SimObject!" << std::endl;
+//        std::cout << "Hello World from SimObject!" << std::endl;
+        DPRINTF(Hello, "Created the hello object\n");
 }
 
 HelloObject *HelloObjectParams::create()
