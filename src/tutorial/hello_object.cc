b003fb2001d6add0a9d05fef0ff9514a0b087e3e
Thomas Copper <universeCopper@gmail.com>
2022-07-05 21:07:10 +0800

    tutorial events, part1
    Change-Id: I13ace24fcd493ca26d82e271fa9f5a17aefa8d6b

--- src/tutorial/hello_object.cc
+++ src/tutorial/hello_object.cc
@@ -4,10 +4,15 @@
 #include "tutorial/hello_object.hh"
 
 HelloObject::HelloObject(HelloObjectParams *params) :
-        SimObject(params)
+        SimObject(params), event([this]{processEvent();}, name())
 {
 //        std::cout << "Hello World from SimObject!" << std::endl;
-        DPRINTF(Hello, "Created the hello object\n");
+        DPRINTF(Hello, "Created the new hello object\n");
+}
+
+void HelloObject::processEvent()
+{
+        DPRINTF(Hello, "Hello World! Processing the Event!\n");
 }
 
 HelloObject *HelloObjectParams::create()
@@ -15,3 +20,7 @@ HelloObject *HelloObjectParams::create()
         return new HelloObject(this);
 }
 
+void HelloObject::startup()
+{
+        schedule(event, 100);
+}
