91427dbfa28fdee911f4da88156b207c466a882b
Thomas Copper <universeCopper@gmail.com>
2022-08-07 01:30:12 +0800

    fix a error: when using bash for var in ls dir, var is path to file
    Change-Id: Ia0aeb5bd072c91574a56d7dc628fa8989dda6dbe

--- gem5withCPU2006-429mcfbench.sh
+++ gem5withCPU2006-429mcfbench.sh
@@ -18,21 +18,25 @@ DRAMSIM3_ini_dir=long-tREFI-configs/ddr4
 #DRAMSIM3_ini_file=DDR4_8Gb_x8_1866.ini
 for DRAMSIM3_ini_file in $(ls ${DRAMSIM3_ini_dir}/*.ini)
 do
-	OUTdir=$OUT_dir_root/$BENCH_NAME/$(basename ${DRAMSIM3_ini_file} .ini)-$(date +%FT%H%M%S)
+	DRAMSIM3_ini_file=$(basename ${DRAMSIM3_ini_file} .ini)
+	
+	OUTdir=$OUT_dir_root/$BENCH_NAME/${DRAMSIM3_ini_file} .ini)-$(date +%FT%H%M%S)
 
 	if [ ! -d "$OUTdir" ]; then
-		echo "per-ini output directory dones't exitst, creating it."
-		mkdir -p $OUTdir
+		echo "per-ini ${OUTdir} "
+		echo "output directory dones't exitst, creating it."
+		mkdir -p "$OUTdir"
 	fi
 
-screen -d -m $GEM5root/build/X86/gem5.opt \
---outdir=$OUTdir \
-$GEM5root/configs/example/se.py \
--c "$BEN_workingdir/$EXEfile" \
--o "$BEN_workingdir/inp.in" \
---cpu-type=TimingSimpleCPU \
---mem-type=DRAMsim3 \
---dramsim3-ini=$DRAMSIM3_ini_dir/${DRAMSIM3_ini_file}
+	screen -d -m \
+	$GEM5root/build/X86/gem5.opt \
+	--outdir="$OUTdir" \
+	$GEM5root/configs/example/se.py \
+	-c "$BEN_workingdir/$EXEfile" \
+	-o "$BEN_workingdir/inp.in" \
+	--cpu-type=TimingSimpleCPU \
+	--mem-type=DRAMsim3 \
+	--dramsim3-ini=$DRAMSIM3_ini_dir/${DRAMSIM3_ini_file}
 	
 	#--debug-flags=MemoryAccess \
 	#-i "$SPEC/$BENCH/$BEN_suffix/inp.in" \
