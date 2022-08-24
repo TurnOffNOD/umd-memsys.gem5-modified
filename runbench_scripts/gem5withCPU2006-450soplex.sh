e5a04a537d491394e9b3d9be3208ae28e07a99bf
Thomas Copper <universeCopper@gmail.com>
2022-08-25 00:27:31 +0800

    newly run on new refresh time
    Change-Id: I6bbaebec5033138c487fa13fe17be82d34603acc

--- runbench_scripts/gem5withCPU2006-450soplex.sh
+++ runbench_scripts/gem5withCPU2006-450soplex.sh
@@ -24,7 +24,7 @@ EXEfile=${BENCH_NAME}_base.amd64-m64-gcc43-nn
 GEM5root=/home/tongxian/Projects/umd-memsys.gem5-modified
 OUT_dir_root=$GEM5root/gem5cpu2006result
 
-DRAMSIM3_ini_dir=long-tREFI-configs/ddr3
+DRAMSIM3_ini_dir=long-tREFI-configs/new_ref
 
 #DRAMSIM3_ini_file=DDR4_8Gb_x8_1866.ini
 for DRAMSIM3_ini_file in ${DRAMSIM3_ini_dir}/*.ini
@@ -39,7 +39,7 @@ do
 		mkdir -p "$OUTdir"
 	fi
 
-	#screen -d -m \
+	screen -d -m \
 	$GEM5root/build/X86/gem5.opt \
 	--outdir="$OUTdir" \
 	$GEM5root/configs/example/se.py \
