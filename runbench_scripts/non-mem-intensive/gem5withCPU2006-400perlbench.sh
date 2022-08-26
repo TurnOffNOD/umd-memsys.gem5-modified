04b41c0144f31fc82c1770de480da4f3c4686cfc
Thomas Copper <universeCopper@gmail.com>
2022-08-26 21:11:59 +0800

    new bench script, a modified old scripts add new refresh interval test
    Change-Id: I5fa74bf578797ec8894008674efbd0045d6e946e

--- runbench_scripts/non-mem-intensive/gem5withCPU2006-400perlbench.sh
+++ runbench_scripts/non-mem-intensive/gem5withCPU2006-400perlbench.sh
@@ -9,13 +9,27 @@
 #/home/tongxian/Projects/umd-memsys.gem5-modified/build/X86/gem5.opt(_Z15print_backtracev+0x15)[0x7378a5]
 #/home/tongxian/Projects/umd-memsys.gem5-modified/build/X86/gem5.opt(_Z12abortHandleri+0x36)[0x7416d6]
 
-cd ~/Projects/umd-memsys.gem5-modified/ || exit
+#New error output after "cd before gem5 execute":
+#warn: instruction 'movntdq_Mo_Vo' unimplemented
+#info: Increasing stack size by one page.
+#panic: Tried to read unmapped address 0xc.
+#PC: 0x48ecc7, Instr:   TEST_M_I : ld   t1d, DS:[rdi + 0xc]
+#Memory Usage: 16886428 KBytes
+#Program aborted at tick 17483938000
+#--- BEGIN LIBC BACKTRACE ---
+#/home/tongxian/Projects/umd-memsys.gem5-modified/build/X86/gem5.opt(_Z15print_backtracev+0x15)[0x7378a5]
+#/home/tongxian/Projects/umd-memsys.gem5-modified/build/X86/gem5.opt(_Z12abortHandleri+0x36)[0x7416d6]
+#/lib/x86_64-linux-gnu/libpthread.so.0(+0x143c0)[0x7f3199a0a3c0]
+#/lib/x86_64-linux-gnu/libc.so.6(gsignal+0xcb)[0x7f319913b03b]
+#/lib/x86_64-linux-gnu/libc.so.6(abort+0x12b)[0x7f319911a859]
+
+
 cd ../spec2006v1.1/ || exit
 source ./shrc
 cd - || exit
 
-BENCH=403.gcc
-BENCH_NAME=gcc
+BENCH=400.perlbench
+BENCH_NAME=perlbench
 BEN_suffix=run/run_base_test_amd64-m64-gcc43-nn.0000
 BEN_workingdir=$SPEC/benchspec/CPU2006/$BENCH/$BEN_suffix
 EXEfile=${BENCH_NAME}_base.amd64-m64-gcc43-nn
@@ -39,45 +53,19 @@ do
 	fi
 
 	cd ${BEN_workingdir} || exit
-	screen -d -m \
+	#screen -d -m \
 	$GEM5root/build/X86/gem5.opt \
 	--outdir="$OUTdir" \
 	$GEM5root/configs/example/se.py \
 	-c "$BEN_workingdir/$EXEfile" \
-	-o "${BEN_workingdir}/cccp.i -o cccp.s" \
+	-o "-I. -I./lib test.pl" \
 	--cpu-type=TimingSimpleCPU \
 	--mem-type=DRAMsim3 \
-	--dramsim3-ini=${GEM5root}/$DRAMSIM3_ini_dir/${DRAMSIM3_ini_file}.ini
+	--dramsim3-ini=${GEM5root}/${DRAMSIM3_ini_dir}/${DRAMSIM3_ini_file}.ini
+
 	cd - || exit
 	
 	#--debug-flags=MemoryAccess \
 	#-i "$SPEC/$BENCH/$BEN_suffix/inp.in" \
 	#--debug-file=$BENCH.debugfile \
-done
-
-
-DRAMSIM3_ini_dir=long-tREFI-configs/new_ref
-for DRAMSIM3_ini_file in ${GEM5root}/${DRAMSIM3_ini_dir}/*.ini
-do
-	DRAMSIM3_ini_file=$(basename ${DRAMSIM3_ini_file} .ini)
-	
-	OUTdir=$OUT_dir_root/$BENCH_NAME/${DRAMSIM3_ini_file}-$(date +%FT%H%M%S)
-
-	if [ ! -d "$OUTdir" ]; then
-		echo "per-ini ${OUTdir} "
-		echo "output directory dones't exitst, creating it."
-		mkdir -p "$OUTdir"
-	fi
-
-	cd ${BEN_workingdir} || exit
-	screen -d -m \
-	$GEM5root/build/X86/gem5.opt \
-	--outdir="$OUTdir" \
-	$GEM5root/configs/example/se.py \
-	-c "$BEN_workingdir/$EXEfile" \
-	-o "${BEN_workingdir}/cccp.i -o cccp.s" \
-	--cpu-type=TimingSimpleCPU \
-	--mem-type=DRAMsim3 \
-	--dramsim3-ini=${GEM5root}/$DRAMSIM3_ini_dir/${DRAMSIM3_ini_file}.ini
-	cd - || exit
 done
